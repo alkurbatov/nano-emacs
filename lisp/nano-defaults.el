293578bd3a7fce276294fe3d382de590ee4e4f87
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2024-02-29 11:47:51 +0300

    👷 (eglot) Adjust performance optimizations to latest changes
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- lisp/nano-defaults.el
+++ lisp/nano-defaults.el
@@ -260,8 +260,12 @@
 (setq-default abbrev-mode t)
 
 ;; Tweak Eglot
-(setq-default eglot-events-buffer-size 0 ; Disable debug log to speed up things a bit
-              eglot-autoshutdown t       ; Automatically shutdown backend if last buffer was killed
+
+;; Disable debug log to speed up things a bit
+(fset #'jsonrpc--log-event #'ignore) ; Remove laggy typing it probably reduces chatty json from lsp to eglot
+(setq eglot-events-buffer-config '(:size 0 :format full))
+
+(setq-default eglot-autoshutdown t       ; Automatically shutdown backend if last buffer was killed
               eglot-sync-connect nil     ; Otherwise, Elgot freezes the UI for ~3s when large file is opened
               eglot-connect-timeout nil) ; Never time out Eglot connection to make things faster
 
@@ -270,8 +274,6 @@
 ;; Specify explicitly to use Orderless for Eglot
 (setq completion-category-overrides '((eglot (styles orderless))
                                       (eglot-capf (styles orderless))))
-
-(fset #'jsonrpc--log-event #'ignore) ; Remove laggy typing it probably reduces chatty json from lsp to eglot
 
 (provide 'nano-defaults)
 ;;; nano-defaults.el ends here
