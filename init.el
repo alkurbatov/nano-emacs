01065da4e8622e27d8ff25329c5ff4193f7f576a
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2024-04-16 12:02:51 +0300

    🐛 Broken lspce install on clean system
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- init.el
+++ init.el
@@ -153,10 +153,9 @@
          :files (:defaults ,(pcase system-type
                               ('gnu/linux "lspce-module.so")
                               ('darwin "lspce-module.dylib")))
-         :pre-build ("cargo" "build" "--release")
-         :post-build ,(pcase system-type
-                        ('gnu/linux '("cp" "./target/release/liblspce_module.so" "./lspce-module.so"))
-                        ('darwin '("cp" "./target/release/liblspce_module.dylib" "./lspce-module.dylib")))))
+         :pre-build ,(pcase system-type
+                       ('gnu/linux '(("cargo" "build" "--release") ("cp" "./target/release/liblspce_module.so" "./lspce-module.so")))
+                       ('darwin '(("cargo" "build" "--release") ("cp" "./target/release/liblspce_module.dylib" "./lspce-module.dylib"))))))
 
 ;; Load settings
 (require 'nano)
