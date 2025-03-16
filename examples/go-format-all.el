ca2cb9ac9a517e8fc32d2ea253d1fbcf851e7339
Alexander Kurbatov <sir.alkurbatov@yandex.ru>
2025-03-16 18:13:53 +0300

    📚 Include extended example ad gci usage
    Signed-off-by: Alexander Kurbatov <sir.alkurbatov@yandex.ru>

--- examples/go-format-all.el
+++ examples/go-format-all.el
@@ -1,3 +1,5 @@
-((nil . ((format-all-formatters . (("Go" goimports gci gofumpt)))))
+((nil . ((format-all-formatters . (("Go" goimports
+                                    (gci "--skip-generated" "--custom-order" "-s" "standard" "-s" "default" "-s" "prefix(github.com/alkurbatov/guppy)" "-s" "blank" "-s" "dot")
+                                    gofumpt)))))
  (go-ts-mode
   (eval add-hook 'before-save-hook #'format-all-buffer nil 'local)))
